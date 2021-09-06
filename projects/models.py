da16161457d65291d3061e7f7d80e97f50b000cc
Shaga Sresthaa <shagasresthaa@gmail.com>
2021-09-06 16:05:38 +0530

    backend changes

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class projectsList(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=10)
+    url = models.URLField()
+    
+    class Meta:
+        db_table = "projects"
