8b6fd86b70c10c99b6760a7cb3adff30f66488fc
Shaga Sresthaa <shagasresthaa@gmail.com>
2021-09-10 20:38:31 +0530

    added blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
+
+class blogsList(models.Model):
+    postId = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=150)
+    blogHeaderImage = ResizedImageField(size=[500, 500], upload_to='blog_pics', default='default.jpg', quality=85, force_format='JPEG')
+    content = models.TextField()
+    publishedDate = models.DateTimeField()
+    isModified = models.BooleanField(default=False)
+    modifiedDate = models.DateTimeField()
+
+    class Meta:
+        db_table = "blogs"
+
+    def __str__(self):
+        return f'{self.title} blogsList'
